661019abd5b469ab6073ac86ef92085928b53ed4
jaynav1 <jnav0005@student.monash.edu>
2025-01-21 15:46:06 +1100

    add day cycle and reformart for vr enivronment

--- Assets/SunCycle.cs
+++ Assets/SunCycle.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DayCycle : MonoBehaviour
+public class SunCycle : MonoBehaviour
 {
+    public float speed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class DayCycle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Rotate the sun around the X axis
+        transform.Rotate(Vector3.right * speed * Time.deltaTime);
     }
 }
